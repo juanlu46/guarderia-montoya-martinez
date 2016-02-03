2ac41fadb42a0c675a54713d9a7bae34e4692702
Celia2106 <celia.martinez.94@gmail.com>
2016-02-03 20:04:11 +0100

    Acabada todos los mostrar formularios, comprobado que todos los objetos pueden darse de alta y no pueden darse duplicados. Validación de todos los formularios. FALTA POR HACER: - metodo de modificar (para todos los objetos). -Ver porque el .remove no funciona en los métodos de dar de baja. -Ver si dejamos el index asi. -Crear el XML -Lo de los alimentos alergicos

--- Guarderia/js/codigo.js
+++ Guarderia/js/codigo.js
@@ -1,35 +1,56 @@
-window.onload=function(){inicio()};
-var guarderia;
-function inicio() {
-    document.getElementById("alumnos").addEventListener("click",mostrarFormsAlumnos,false);
-    document.getElementById("profesores").addEventListener("click",mostrarFormsProf,false);
+    window.onload=function(){inicio()};
+    var guarderia;
+    function inicio() {
+        document.getElementById("alumnos").addEventListener("click",mostrarFormsAlumnos,false);
+        document.getElementById("profesores").addEventListener("click",mostrarFormsProf,false);
+        document.getElementById("actividades").addEventListener("click",mostrarFormsAct,false);
+        document.getElementById("comedor").addEventListener("click",mostrarFormsComed,false);
 
-  /*
-    document.getElementById("btnAltaAct").addEventListener("click",validarAltaAct,false);
-    document.getElementById("btnModAct").addEventListener("click",validarModAct,false);
-    document.getElementById("btnBajaAct").addEventListener("click",validarBajaAct,false);*/
+         guarderia=new Guarderia();
+    }
+    function mostrarFormsAlumnos(){
+        ocultar("menuProf");
+        ocultar("menuAct");
+        ocultar("menuComed");
+        $("form").hide("normal");
+        document.getElementById("menuAlum").classList.remove("oculto");
+        document.getElementById("mostrarFormAltaAlum").addEventListener("click",mostrarFormAltaAlum,false);
+        document.getElementById("mostrarFormModAlum").addEventListener("click",mostrarFormModAlum,false);
+        document.getElementById("mostrarFormBajAlum").addEventListener("click",mostrarFormBajAlum,false);
+    }
 
-     guarderia=new Guarderia();
-}
-function mostrarFormsAlumnos(){
-    ocultar("menuProf");
-    $("form").hide("normal");
-    document.getElementById("menuAlum").classList.remove("oculto");
-    document.getElementById("mostrarFormAltaAlum").addEventListener("click",mostrarFormAltaAlum,false);
-    document.getElementById("mostrarFormModAlum").addEventListener("click",mostrarFormModAlum,false);
-    document.getElementById("mostrarFormBajAlum").addEventListener("click",mostrarFormBajAlum,false);
-}
-function ocultar(elemento){
+    function mostrarFormsProf(){
+        ocultar("menuAlum");
+        ocultar("menuAct");
+        ocultar("menuComed");
+        $("form").hide("normal");
+        document.getElementById("menuProf").classList.remove("oculto");
+        document.getElementById("mostrarFormAltaProf").addEventListener("click", mostrarFormAltaProf, false);
+        document.getElementById("mostrarFormModProf").addEventListener("click", mostrarFormModProf, false);
+        document.getElementById("mostrarFormBajProf").addEventListener("click", mostrarFormBajProf, false);
+    }
+    function mostrarFormsAct(){
+        ocultar("menuProf");
+        ocultar("menuAlum");
+        ocultar("menuComed");
+        $("form").hide("normal");
+        document.getElementById("menuAct").classList.remove("oculto");
+        document.getElementById("mostrarFormAltaAct").addEventListener("click", mostrarFormAltaAct, false);
+        document.getElementById("mostrarFormModAct").addEventListener("click", mostrarFormModAct, false);
+        document.getElementById("mostrarFormBajAct").addEventListener("click", mostrarFormBajAct, false);
+    }
+    function mostrarFormsComed(){
+        ocultar("menuProf");
+        ocultar("menuAlum");
+        ocultar("menuAct");
+        $("form").hide("normal");
+        document.getElementById("menuComed").classList.remove("oculto");
+        document.getElementById("mostrarFormAltaComed").addEventListener("click", mostrarFormAltaComed, false);
+        document.getElementById("mostrarFormModComed").addEventListener("click", mostrarFormModComed, false);
+        document.getElementById("mostrarFormBajComed").addEventListener("click", mostrarFormBajComed, false);
+    }
+    function ocultar(elemento){
     document.getElementById(elemento).classList.add("oculto");
-}
-
-function mostrarFormsProf(){
-    ocultar("menuAlum");
-    document.getElementById("menuProf").classList.remove("oculto");
-    document.getElementById("mostrarFormAltaProf").addEventListener("click", mostrarFormAltaProf, false);
-    document.getElementById("mostrarFormModProf").addEventListener("click", mostrarFormModProf, false);
-    document.getElementById("mostrarFormBajProf").addEventListener("click", mostrarFormBajProf, false);
-
 }
     function validarFormAltaAlum(){
         var sMensajeError="";
@@ -269,41 +290,149 @@ function mostrarFormsProf(){
         }
 }
 
+    function validarAltaBono(){
+        var sMensajeError="";
+        var todoOk=true;
+        var oBonoActual=null;
 
+        if(!/^(([A-Z]\d{8})|(\d{8}[A-Z])|(\d{8}[a-z]))$/.test(form_altaBono.text_alumno.value)){
+            sMensajeError="Dni incorrecto\n";
+            todoOk=false;
+        }
+        if(form_altaBono.text_horario.value==""){
+            sMensajeError+="Introduzca el horario\n";
+            todoOk=false;
+        }
+       /* LO DEJAMOS SIN VALIDAR PARA QUE PUEDA DEJARLO EN BLANCO SI NO TIENE?*/
+        if(form_altaBono.text_alimentos.value==""){
+            sMensajeError+="Introduzca los alimentos a los que es alérgico. Si no tiene alergías introduzca ninguno \n";
+            todoOk=false;
+        }
+        if(todoOk==false)
+            alert(sMensajeError);
+        else {
+            var oAlumno=guarderia.buscarAlumno(form_altaBono.text_alumno.value);
+            if(oAlumno!=null) {
+                oBonoActual = new BonoComedor(oAlumno, form_altaBono.text_horario.value);
+                alert(guarderia.altaBonoComedor(oBonoActual));
+                form_altaBono.text_alumno.value = "";
+                form_altaBono.txt_horario.value = "";
+                form_altaBono.text_alimentos.value = "";
+            }
+            else
+                alert("Este alumno no existe");
+        }
+    }
+
+    function validarModBono(){
+        var sMensajeError="";
+        var todoOk=true;
+        var oBonoActual=null;
+
+        if(!/^(([A-Z]\d{8})|(\d{8}[A-Z])|(\d{8}[a-z]))$/.test(form_modBono.text_alumno.value)){
+            sMensajeError="Dni incorrecto\n";
+            todoOk=false;
+        }
+        if(form_modBono.text_horario.value==""){
+            sMensajeError+="Introduzca el horario\n";
+            todoOk=false;
+        }
+        /* LO DEJAMOS SIN VALIDAR PARA QUE PUEDA DEJARLO EN BLANCO SI NO TIENE?*/
+        if(form_modBono.text_alimentos.value==""){
+            sMensajeError+="Introduzca los alimentos a los que es alérgico. Si no tiene alergías introduzca ninguno \n";
+            todoOk=false;
+        }
+        if(todoOk==false)
+            alert(sMensajeError);
+        else {
+            var oAlumno=guarderia.buscarAlumno(form_modBono.text_alumno.value);
+            if(oAlumno!=null) {
+               //FALTAAAA MODIFICAR BONO
+                form_altaBono.text_alumno.value = "";
+                form_altaBono.txt_horario.value = "";
+                form_altaBono.text_alimentos.value = "";
+            }
+            else
+                alert("Este alumno no existe");
+        }
+    }
+
+    function validarBajaBono(){
+        var sMensajeError="";
+        var todoOk=true;
+
+        if(!/^(([A-Z]\d{8})|(\d{8}[A-Z])|(\d{8}[a-z]))$/.test(form_bajaBono.text_alumno.value)){
+            sMensajeError="Dni incorrecto\n";
+            todoOk=false;
+        }
+        if(todoOk==false)
+            alert(sMensajeError);
+        else {
+            alert(guarderia.bajaBonoComedor(form_bajaBono.text_alumno.value));
+            form_bajaAct.text_alumno.value="";
+        }
+    }
 
     function mostrarFormAltaProf(){
-        document.getElementById("form_altaProf").classList.remove("oculto");
-        document.getElementById("form_modProf").classList.add("oculto");
-        document.getElementById("form_bajaProf").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_altaProf").show("normal");
         document.getElementById("btnAltaProf").addEventListener("click", validarFormProf, false);
     }
     function mostrarFormModProf(){
-        document.getElementById("form_modProf").classList.remove("oculto");
-        document.getElementById("form_altaProf").classList.add("oculto");
-        document.getElementById("form_bajaProf").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_modProf").show("normal");
         document.getElementById("btnModProf").addEventListener("click",validarFormModProf,false);
     }
     function mostrarFormBajProf(){
-        document.getElementById("form_bajaProf").classList.remove("oculto");
-        document.getElementById("form_altaProf").classList.add("oculto");
-        document.getElementById("form_modProf").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_bajaProf").show("normal");
         document.getElementById("btnBajaProf").addEventListener("click",validarFormBajaProf,false);
     }
+
     function mostrarFormAltaAlum(){
-        document.getElementById("form_altaAlum").classList.remove("oculto");
-        document.getElementById("form_modAlum").classList.add("oculto");
-        document.getElementById("form_bajaAlum").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_altaAlum").show("normal");
         document.getElementById("btnAltaAlum").addEventListener("click",validarFormAltaAlum,false);
     }
     function mostrarFormModAlum(){
-        document.getElementById("form_modAlum").classList.remove("oculto");
-        document.getElementById("form_altaAlum").classList.add("oculto");
-        document.getElementById("form_bajaAlum").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_modAlum").show("normal");
         document.getElementById("btnModAlum").addEventListener("click",validarFormModAlum,false);
     }
     function mostrarFormBajAlum(){
-    document.getElementById("form_bajaAlum").classList.remove("oculto");
-    document.getElementById("form_altaAlum").classList.add("oculto");
-    document.getElementById("form_modAlum").classList.add("oculto");
+        $("form").hide("normal");
+        $("#form_bajaAlum").show("normal");
     document.getElementById("btnBajaAlum").addEventListener("click",validarBajaAlum,false);
 }
+
+    function mostrarFormAltaAct(){
+        $("form").hide("normal");
+        $("#form_altaAct").show("normal");
+        document.getElementById("btnAltaAct").addEventListener("click",validarAltaAct,false);
+    }
+    function mostrarFormModAct(){
+        $("form").hide("normal");
+        $("#form_modAct").show("normal");
+        document.getElementById("btnModAct").addEventListener("click",validarModAct,false);
+    }
+    function mostrarFormBajAct(){
+        $("form").hide("normal");
+        $("#form_bajaAct").show("normal");
+        document.getElementById("btnBajaAct").addEventListener("click",validarBajaAct,false);
+    }
+
+    function mostrarFormAltaComed(){
+        $("form").hide("normal");
+        $("#form_altaBono").show("normal");
+        document.getElementById("btnAltaBono").addEventListener("click",validarAltaBono,false);
+    }
+    function mostrarFormModComed(){
+        $("form").hide("normal");
+        $("#form_modBono").show("normal");
+        document.getElementById("btnModBono").addEventListener("click",validarModBono,false);
+    }
+    function mostrarFormBajComed(){
+        $("form").hide("normal");
+        $("#form_bajaBono").show("normal");
+        document.getElementById("btnBajaBono").addEventListener("click",validarBajaBono,false);
+    }
