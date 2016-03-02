57e74df6fa9b2b03832d8c3ef894c94ce04b3ab9
Juan Luis Montoya <juanlu46@gmail.com>
2016-03-02 17:45:42 +0100

    Terminado php's de tramites

--- Guarderia/php/altaNota.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Juanlu
- * Date: 02/03/2016
- * Time: 17:34
- */
---
+++ Guarderia/php/tramites/altaNota.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/javascript');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+
+$mySQLi=new mysqli("localhost","root","","guarderia");
+$mySQLi->query("SET NAMES utf8");
+$oAlumno=json_decode($_REQUEST['datos']);
+$sql="INSERT INTO notas VALUES('".$_REQUEST['dni']."','".$_REQUEST['evaluacion']."','".$_REQUEST['curso']."','".
+    $_REQUEST['materia1']."','".$_REQUEST['nota1']."','".$_REQUEST['materia2']."','".$_REQUEST['nota2']."','".
+    $_REQUEST['materia3']."','".$_REQUEST['nota3']."')";
+$mensaje="$('<div title=\"Introducir notas\">";
+if($mySQLi->query($sql))
+    $mensaje.="Se han introducido con éxito las notas";
+else
+    $mensaje.="Se ha producido un error: ".$mySQLi->errno."-".$mySQLi->error;
+$mensaje.=").dialog();";
+
+echo $mensaje;
