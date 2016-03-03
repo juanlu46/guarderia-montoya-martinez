528c4db4db4f81a96b60d65869dd9a70fda7f8f3
Juan Luis Montoya <juanlu46@gmail.com>
2016-03-03 17:53:15 +0100

    LIMPIEZA

--- Guarderia/php/tramites/altaProfesor.php
+++ Guarderia/php/tramites/altaProfesor.php
@@ -5,13 +5,13 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 
 $mySQLi=new mysqli("localhost","root","","guarderia");
 $mySQLi->query("SET NAMES utf8");
-$oProfesor=json_decode($_REQUEST['datos']);
-$sql="INSERT INTO profesor VALUES('".$oProfesor->dni."','".
-    $oProfesor->nombre."','".$oProfesor->apellidos."',".$oProfesor->telefono.",'".$oProfesor->direccion."','".$oProfesor->grupo."')";
+$sql="INSERT INTO profesor VALUES('".$_REQUEST['dni']."','".
+    $_REQUEST['nombre']."','".$_REQUEST['apellidos']."',".$_REQUEST['telefono'].
+    ",'".$_REQUEST['grupo']."')";
 $mensaje="$('<div title=\"Alta Profesor\">";
 if($mySQLi->query($sql))
-    $mensaje.="Se ha dado de alta con éxito al profesor ".$oProfesor->nombre." ".
-        $oProfesor->apellidos;
+    $mensaje.="Se ha dado de alta con éxito al profesor ".$_REQUEST['nombre']." ".
+        $_REQUEST['apellidos'];
 else
     $mensaje.="Se ha producido un error: ".$mySQLi->errno."-".$mySQLi->error;
 $mensaje.=").dialog();";
