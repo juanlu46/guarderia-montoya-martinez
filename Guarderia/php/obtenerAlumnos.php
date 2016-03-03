95829dffc700755395549ee541e2c0e3e9f85244
Celia2106 <celia.martinez.94@gmail.com>
2016-03-03 20:05:51 +0100

    .

--- Guarderia/php/obtenerAlumnos.php
+++ Guarderia/php/obtenerAlumnos.php
@@ -1,15 +1,15 @@
 <?php
-header('Content-Type: application/xml');
+header('Content-Type: text/xml');
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 $mySQLi=new mysqli("localhost","root","","guarderia");
 $mySQLi->query("SET NAMES utf8");
 $select=$mySQLi->query("SELECT * FROM alumnos");
-$xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+$xml="<?xml version=\"1.0\" encoding=\"UTF-8\"?><alumnos>";
 while($array=$select->fetch_assoc()){
     $xml.="<alumno dni='".$array['DNI']."'><nombre>".$array['NOMBRE']."</nombre>";
     $xml.="<apellidos>".$array['APELLIDOS']."</apellidos><edad>".$array['EDAD']."</edad>";
     $xml.="<contacto>".$array['CONTACTO']."</contacto><direccion>".$array['DIRECCION']."</direccion>";;
     $xml.="<grupo>".$array['GRUPO']."</grupo></alumno>";
 }
-echo $xml;
+echo $xml."</alumnos>";
